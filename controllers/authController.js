3fcffc3a462b0e88f9c2eecb520cef69d180cc34
fsdlds <mjnh0915@naver.com>
2019-11-17 15:11:41 +0900

    solve signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,26 +1,14 @@
 const { User } = require("../models");
 
-// 주석 처리된 부분은 어디 라우터인지 모르겠음ㅜㅜ (수정 바람)
-// calendar가 home 역할해서 home 필요없을 것 같았는데 이부분 잘모르겠어서!
-/*
-router.get(routes.home, (req, res) => {
-    if (req.session.user) {
-        res.redirect(routes.calendar);
-    } else {
-        res.redirect(routes.join);
-    }
-});
-*/
 
-//이것도 잘 모르겠어서 주석해놓음!
-/*
-router.get("/addAlarm", (req, res) => {
-    res.render("alarm", {
-        title: "Mediger-MediInput",
-        user: null
-    });
-});
-*/
+const home = (req, res) => {// Mediger 띄워지는 가장 첫 페이지 join 되어 있는지 확인한다.
+    if (req.session.user) {
+        res.redirect("/calendar");
+    } else {
+        res.redirect("/join");
+    }
+};
+
 
 const getJoin = (req, res) => {
     res.render("join", {
@@ -36,15 +24,12 @@ const postJoin = async (req, res) => {
             userName: name,
             birth: birthDay,
             sex: sex
+        }).then(user => {
+            req.session.user = {
+                userID : user.dataValues.userID
+            };
         });
-        const userId = await User.findOne({
-            attributes: ["userId"],
-            where: { userName: name, birth: birthDate }
-        });
-        req.session.user = {
-            id: userId["dataValues"]["userId"]
-        };
-        res.redirect(routes.calendar);
+        res.redirect("/calendar");
     } catch (error) {
         console.error(error);
         return next(error);
@@ -52,6 +37,7 @@ const postJoin = async (req, res) => {
 };
 
 module.exports = {
+    home,
     getJoin,
     postJoin
 };
--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,8 +1,10 @@
+const calendar = (req, res) => {res.render("calendar");};
 const calendarDetail = (req, res) => {};
 const alarmDetail = (req, res) => {};
 const alarmRecord = (req, res) => {};
 
 module.exports = {
+    calendar,
     calendarDetail,
     alarmDetail,
     alarmRecord
