fdd64cf9f8be6456785baf0a14b573fbf133c1f2
fsdlds <mjnh0915@naver.com>
2019-11-18 03:25:15 +0900

    calendarDetail

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,28 +1,39 @@
 const {Schedule} = require("../models");
+const {Op} = require("sequelize");
 
 const calendar = (req, res) => {res.render("calendar");};
 const calendarDetail =  async (req, res) => {
     try{
-        const date = new Date(req.params.date).getDate();
-        console.log(date);
+        req.session.user.userID = 45; //나중에 지울 것 이건 테스트를 위한 거니까
+        const today = new Date();
+        const dateString = String(today.getFullYear())+"-"+String(today.getMonth()+1)+"-"+req.params.date;
+        const date = new Date(dateString);
+        const alarmInfo = [];
         const alarmList = await Schedule.findAll({
             where: {
                 userID: req.session.user.userID,
-                startDate: {lte: date},
-                endDate: {gte: date}
+                startDate: {[Op.lte]: date},
+                endDate: {[Op.gte]: date}
             },
-            attributes: ["scheID","scheName","dose","startDate","endDate","scheHour","scheMin"]
+            attributes: ["scheID","scheName","dose","scheHour","scheMin"]
         });
+        for (let schedule of alarmList)
+        {
+            var scheInfo = schedule.dataValues.scheName+" "+schedule.dataValues.scheHour+":"+schedule.dataValues.scheMin;
+            alarmInfo.push([scheInfo,schedule.dataValues.scheID])
+        }
         res.render("calendar",{
-        alarmList: "Hello"
+        userDate: req.params.date,
+        alarmList: alarmInfo
         });
-    }catch(error)
-    {
+    }catch(error){
         console.error(error);
         return next(error);
     }
 };
-const alarmDetail = (req, res) => {};
+const alarmDetail = (req, res) => {
+    res.render("alarmDetail");
+};
 const alarmRecord = (req, res) => {};
 
 module.exports = {
--- routers/calendarRouter.js
+++ routers/calendarRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("", calendar);//기본 캘린더 페이지
 router.get("/:date", calendarDetail); //해당 일자에 해당하는 알람 리스트 보여줌. req.params.date로 처리
-router.get("/{date}/:id/alarm", alarmDetail); //해당일 개별 약 알람 보기 -> id = 약id
-router.post("/{date}/:id/alarm", alarmRecord); //해당일 개별 약 알람 기록 : 건너뜀/재알람/먹음 -> id = 약id
+router.get("/:date/:id", alarmDetail); //해당일 개별 알람에 대한 복용 여부 페이지 보여줌. id는 알람 id
+router.post("/:date/:id/alarm", alarmRecord); //해당일 개별 약 알람 기록 : 건너뜀/재알람/먹음 -> id = 약id
 
 module.exports = router;
