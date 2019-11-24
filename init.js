320e46eba1edf34658ae8859df620e5359aba157
Yewon-july <kellychung730@gmail.com>
2019-11-24 19:00:21 +0900

    add connection

--- init.js
+++ init.js
@@ -1,8 +1,19 @@
 var app = require("./app");
+var mysql = require('mysql2');
 
-const PORT = process.env.PORT || 2000;
+app.use(express.logger())
+
+var connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b0bb2113fafd66',
+    password: '133d3d26',
+    database: 'heroku_6239ce1834731ad',
+});
+
+connection.connect();
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () => 
-    console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Listening on: ${PORT}`);
 
 app.listen(PORT,handleListening);
